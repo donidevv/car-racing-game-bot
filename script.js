6c864bf442860e44b2f0dd5810b06df4142a0e29
donidevv <dmuxiddinov30@gmail.com>
2024-09-25 10:04:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('startButton').addEventListener('click', function() {
+       document.getElementById('game').style.display = 'block';
+       alert('Game Started!'); // Replace this with your actual game logic
+   });
+   
